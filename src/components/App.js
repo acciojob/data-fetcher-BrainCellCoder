6d9eaac973f85f2629f4df521ead0e080d148f86
BrainCellCoder <00abhisheksharma1999@gmail.com>
2024-10-07 09:42:03 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [data, setData] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = () => {
+    setLoading(true);
+    fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then((json) => {
+        setData(JSON.stringify(json));
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error("Error fetching data:", err);
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      {loading && <p>Loading...</p>}
+      {data && data}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
